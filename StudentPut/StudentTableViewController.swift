9eadb7447c4498d3251ed2ad4e969db22cbf26c9
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-04-12 10:31:26 -0600

    Add StudentTableViewController Subclass

---
+++ StudentPut/StudentTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  StudentTableViewController.swift
+//  StudentPut
+//
+//  Created by Bradley GIlmore on 4/12/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class StudentTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+}
